40447de1d1f74eb99720c4c2d87c905317688a5a
Dasaru <dberry02jr@yahoo.com>
2024-05-18 18:15:45 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import './styles/App.css';
+
+function App() {
+  return (
+    <>
+      <h1>CV Application</h1>
+      <GeneralInfo />
+      <Education />
+      <Experience />
+    </>
+  )
+}
+
+export default App
